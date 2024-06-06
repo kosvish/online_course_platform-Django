a4a387cf8350df80dbe48369b2c2449b374fb781
mrkoc <mr.kocta@bk.ru>
2024-06-06 23:21:24 +0300

    (create): register my model in django admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Course
+from django.contrib.auth import settings
+from users.models import User
 
-# Register your models here.
+
+class CourseAdminInline(admin.TabularInline):
+    model = User
+    extra = 1
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructor')
+    inlines = [CourseAdminInline]
