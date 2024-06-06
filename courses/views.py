e00eca1072632d491cc6f1654c6f99587a80d800
mrkoc <mr.kocta@bk.ru>
2024-06-06 23:23:26 +0300

    (create): views for list, and detail view course

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Course
 
-# Create your views here.
+
+class CourseListView(generic.ListView):
+    model = Course
+
+
+class CourseDetailView(generic.DetailView):
+    model = Course
