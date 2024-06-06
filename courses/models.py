59bd92fd01b487cd803b5fe09b31b4c719f275c1
mrkoc <mr.kocta@bk.ru>
2024-06-06 22:09:26 +0300

    (create): model for courses

---
+++ courses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, help_text="Введите название вашего курса")
+    description = models.TextField(help_text="Введите описание вашего курса")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    instructor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
