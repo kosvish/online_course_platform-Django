616ad127a53888aae222c8bae881cb4f83e17367
mrkoc <mr.kocta@bk.ru>
2024-06-08 14:17:57 +0300

    (delete): extra Class for Inline

--- courses/admin.py
+++ courses/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from .models import Course
 from django.contrib.auth import settings
-from users.models import User
-
-
-class CourseAdminInline(admin.TabularInline):
-    model = User
-    extra = 1
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'instructor')
-    inlines = [CourseAdminInline]
