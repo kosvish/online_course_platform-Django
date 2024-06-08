08a9a9f68062660491f66b8936cdc47c124697f5
mrkoc <mr.kocta@bk.ru>
2024-06-08 15:03:01 +0300

    (create): include new url path for courses and create main page

--- online_course_platform/urls.py
+++ online_course_platform/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from courses.views import index
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
-    # path('', index, name='main')
+    path('admin/', admin.site.urls),
+    path('courses/', include('courses.urls'), name='courses'),
+    path('', index, name='main')
 ]
