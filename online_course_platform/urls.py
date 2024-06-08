d4b231f18a78e33dc9746fd963e5911b1f8734e1
mrkoc <mr.kocta@bk.ru>
2024-06-08 21:41:47 +0300

    (add): url for media images

--- online_course_platform/urls.py
+++ online_course_platform/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from courses.views import index
-
+from django.contrib.auth import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('courses.urls'), name='courses'),
     path('', index, name='main')
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
