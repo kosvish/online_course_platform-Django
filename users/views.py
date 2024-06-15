da0a9020246d9e370ef32006c04dfc484e3ab0a8
mrkoc <mr.kocta@bk.ru>
2024-06-16 00:17:04 +0300

    (create): 2 test function to view reg and log forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import UserRegistrationForm, UserLoginForm
 
-# Create your views here.
+
+def registration(request):
+    form = UserRegistrationForm()
+    return render(request, 'users/registration.html', context={"request": request, 'form': form})
+
+
+def login(request):
+    form = UserLoginForm()
+    return render(request, 'users/login.html', context={'request': request, 'form': form})
