286b4f3b4da201346202e910e533c0b4e5c60407
mrkoc <mr.kocta@bk.ru>
2024-06-16 00:15:49 +0300

    (add): app_name, and categories url path

--- courses/urls.py
+++ courses/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 
+app_name = 'courses'
 
 urlpatterns = [
     path('all/', CourseListView.as_view(), name='course-all'),
-    path('<int:pk>/', CourseDetailView.as_view(), name='course-detail')
+    path('<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
+    path('categories/', CourseDetailView.as_view(), name='categories'),
 ]
