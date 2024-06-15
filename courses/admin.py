74913dc72e175a4d81b5424a55ba7d61f8837de3
mrkoc <mr.kocta@bk.ru>
2024-06-16 00:12:34 +0300

    (add): register CategoryAdmin model

--- courses/admin.py
+++ courses/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Category
 from django.contrib.auth import settings
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'instructor')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
