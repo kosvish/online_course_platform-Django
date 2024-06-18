ac527be154fdf15608bfbe1d86acabad8cec967a
mrkoc <mr.kocta@bk.ru>
2024-06-18 23:38:44 +0300

    (create): add phone, mail, is_verified fields. Also create method to get tokens

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class User(AbstractUser):
@@ -18,5 +19,18 @@ class User(AbstractUser):
         related_query_name='custom_user_permission'
     )
     image = models.ImageField(upload_to="user_images", blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    phone_number = models.IntegerField(blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return ({
+            'refresh': str(refresh),
+            'refresh': str(refresh.access_token),
+        })
 
 
