e91ed35e2a5edf715afce809b147fd483bd9bd14
mrkoc <mr.kocta@bk.ru>
2024-06-18 23:40:30 +0300

    (add): url path to signup, and verify email

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import registration, login, profile
+from .views import registration, login, profile, SingUp, VerifyEmail
 
 app_name = 'users'
 
 urlpatterns = [
     path('registration/', registration, name='registration'),
     path('login/', login, name='login'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+]
+
+urlpatterns += [
+    path('', SingUp.as_view(), name='signup'),
+    path('email-verify/', VerifyEmail.as_view(), name='email-verify')
 ]
